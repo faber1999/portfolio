5dbc6909504a223a7f2647dd5a871fcbb4c5987c
Faber Grajales <fabergrajales1999@gmail.com>
2024-10-31 11:05:50 -0500

    Se agregan algunos valores para manejar el box-shadow inset

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,15 @@ export default {
         'sub-primary': '#4ed1db',
         'post-primary': '#13cfe1',
         secondary: '#282828'
+      },
+      boxShadow: {
+        inner: 'inset 0 0px 4px 0 rgba(0, 0, 0, 0.6)',
+        'inner-md': 'inset 0 0px 8px 0 rgba(0, 0, 0, 0.6)',
+        'inner-lg': 'inset 0 0px 12px 0 rgba(0, 0, 0, 0.6)',
+        'inner-xl': 'inset 0 0px 16px 0 rgba(0, 0, 0, 0.6)',
+        'inner-2xl': 'inset 0 0px 20px 0 rgba(0, 0, 0, 0.6)',
+        'inner-3xl': 'inset 0 0px 24px 0 rgba(0, 0, 0, 0.6)',
+        'inner-full': 'inset 0 0px 32px 0 rgba(0, 0, 0, 0.6)'
       }
     }
   }
